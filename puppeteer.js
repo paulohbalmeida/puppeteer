e5eb5b43914dcbd7c54867e11ca0854b03d932a4
Paulo Henrique Barros de Almeida <paulohbalmeida@gmail.com>
2020-05-25 10:40:52 -0300

    Update

--- puppeteer.js
+++ puppeteer.js
@@ -1,14 +1,14 @@
-const puppeteer = require(‘puppeteer’)async function savePDF( file_path, html ) {    try{
+const puppeteer = require('puppeteer')async function savePDF( file_path, html ) {    try{
         let options = {
             headless: true,
-            args: [‘--disable-dev-shm-usage’,‘--no-sandbox’]
+            args: ['--disable-dev-shm-usage','--no-sandbox']
         }
-        if(process.env.NODE_ENV ==‘production’){
-            options.executablePath = ‘/usr/bin/chromium-browser’;
+        if(process.env.NODE_ENV =='production'){
+            options.executablePath = '/usr/bin/chromium-browser';
         }        const browser = await puppeteer.launch(options);
         const page = await browser.newPage();
-        await page.setContent( html, { waitFor: ‘networkidle2’ } )
-        const pdf = await page.pdf({ format: ‘A4’ });
+        await page.setContent( html, { waitFor: 'networkidle2' } )
+        const pdf = await page.pdf({ format: 'A4' });
         await browser.close();
         return pdf
     }catch(e){
